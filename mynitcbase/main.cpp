5a25f50bbc6b34b93e63959db4c48247b7c8434b
Ajay Kumar <ajaykmr1501@gmail.com>
2024-07-26 15:34:52 +0530

    After doing full setup and checking disk working

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -3,11 +3,23 @@
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
 
-int main(int argc, char *argv[]) {
-  /* Initialize the Run Copy of Disk */
-  Disk disk_run;
-  // StaticBuffer buffer;
-  // OpenRelTable cache;
+//local headers
+#include <iostream>
 
-  return FrontendInterface::handleFrontend(argc, argv);
+int main(int argc, char *argv[]) {
+  Disk disk_run;
+
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer, 7000);
+  char message[] = "hello";
+  memcpy(buffer + 20, message, 6);
+  Disk::writeBlock(buffer, 7000);
+
+  unsigned char buffer2[BLOCK_SIZE];
+  char message2[6];
+  Disk::readBlock(buffer2, 7000);
+  memcpy(message2, buffer2 + 20, 6);
+  std::cout << message2 <<'\n';
+
+  return 0;
 }
